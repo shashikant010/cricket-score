464f77a50b98aef66f010fc0857edfb9e8662ebe
shashikant010 <shashikantyadav0503@gmail.com>
2024-01-28 20:37:48 +0530

    added keyboard shortcut to add score and tried to switch turn(failed)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let body=document.querySelector("body");
 let team1score=document.querySelector(".team1 .teamscore .score")
 let team1wicket=document.querySelector(".team1 .teamscore .wicket")
 let team1over=document.querySelector(".team1 .socry .overs .over")
@@ -33,16 +34,29 @@ let run2=document.querySelectorAll(".edit button")
 
 let team1turn=true;
 let team2turn=false;
-const checkturnchange=()=>{
-if(team1over.innerText=='20'||team1wicket.innerText=='10'){
+const checkturn1change=()=>{
+// if(team1over.innerText==='20'||team1wicket.innerText==='10'){
+//     team1turn=false;
+//     team2turn=true;
+if(team1over.innerText==="10"){
     team1turn=false;
     team2turn=true;
-}
+    console.log("turn should change")
 }
 
-if(team1turn){
+}
+const checkturn2change=()=>{
+    // if(team1over.innerText==='20'||team1wicket.innerText==='10'){
+    //     team1turn=false;
+    //     team2turn=true;
+    if(team1over.innerText==="10"){
+        team2turn=false;
+        console.log("turn should change")
+    }
+    
+    }
 
-const increaseOver=()=>{
+const increaseOver1=()=>{
     ball1++;
 team1ball.innerText=ball1;
 if(ball1==6){
@@ -54,25 +68,66 @@ if(ball1==6){
 }
 }
 
+if(team1turn){
 
 for (let i = 0; i <= 5; i++) {
     run1[i].addEventListener("click",()=>{
         team1score.innerText=parseInt(team1score.innerText)+parseInt(run1[i].innerText);
-        increaseOver();
-        checkturnchange();
+        increaseOver1();
+        checkturn1change();
     })
 }
 
 edit1Wick.addEventListener("change",()=>{
     team1wicket.innerText++;
-    checkturnchange();
+    checkturn1change();
 })
+
+body.addEventListener('keypress', function(event) {
+
+
+    if (event.key === "1") {
+        team1score.innerText=parseInt(team1score.innerText)+ 1;
+          increaseOver1();
+          checkturn1change();
+      }
+
+      if (event.key === "2") {
+        team1score.innerText=parseInt(team1score.innerText)+ 2;
+          increaseOver1();
+          checkturn1change();
+      }
+
+      if (event.key === "3") {
+        team1score.innerText=parseInt(team1score.innerText)+ 3;
+          increaseOver1();
+          checkturn1change();
+      }
+
+      if (event.key === "4") {
+        team1score.innerText=parseInt(team1score.innerText)+ 4;
+          increaseOver1();
+          checkturn1change();
+      }
+
+      if (event.key === "6") {
+        team1score.innerText=parseInt(team1score.innerText)+ 6;
+          increaseOver1();
+          checkturn1change();
+      }
+
+      if (event.key === "0") {
+        team1score.innerText=parseInt(team1score.innerText)+ 0;
+          increaseOver1();
+          checkturn1change();
+      }
+    
+
+
+  });
 
 
 }
-
-
-
 
 //team2 settings
 
@@ -108,7 +163,58 @@ if(team2turn){
         team2wicket.innerText++;
     })
 
+    body.addEventListener('keypress', function(event) {
+
+
+        if (event.key === "1") {
+            team2score.innerText=parseInt(team2score.innerText)+ 1;
+              increaseOver();
+              checkturn2change();
+          }
+    
+          if (event.key === "2") {
+            team2score.innerText=parseInt(team2score.innerText)+ 2;
+              increaseOver1();
+              checkturn2change();
+          }
+    
+          if (event.key === "3") {
+            team2score.innerText=parseInt(team2score.innerText)+ 3;
+              increaseOver1();
+              checkturn2change();
+          }
+    
+          if (event.key === "4") {
+            team2score.innerText=parseInt(team2score.innerText)+ 4;
+              increaseOver1();
+              checkturn2change();
+          }
+    
+          if (event.key === "6") {
+            team2score.innerText=parseInt(team2score.innerText)+ 6;
+              increaseOver1();
+              checkturn2change();
+          }
+    
+          if (event.key === "0") {
+            team2score.innerText=parseInt(team2score.innerText)+ 0;
+              increaseOver1();
+              checkturn2change();
+          }
+        
+    
+    
+      });
+
 }
 
+
+
+const turnchange=()=>{
+    if(team1turn){
+        team1turn=0;
+    }
+    
+}
 
 
